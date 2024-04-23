a1fcbe5c78665a57bf18ac1b035d7e265b7610f7
Chien0127 <erica12793@gmail.com>
2024-04-23 21:43:53 +0800

    0423

--- RadioButton1/app/src/main/java/com/example/radiobutton1/MainActivity.java
+++ RadioButton1/app/src/main/java/com/example/radiobutton1/MainActivity.java
@@ -5,40 +5,76 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View v) {
-                String outputStr = "";
-                RadioButton rdbBoy = (RadioButton)  findViewById(R.id.rdbBoy);
-                RadioButton rdbGirl = (RadioButton)  findViewById(R.id.rdbGirl);
-                if (rdbBoy.isChecked())
-                    outputStr += getResources().getString(R.string.male) + "\n";
-                else if (rdbGirl.isChecked())
-                    outputStr += getResources().getString(R.string.female) + "\n";
+        final EditText editTextNumber = findViewById(R.id.editTextNumber);
+        final TextView lblOutput = findViewById(R.id.lblOutput);
+        final Button button = findViewById(R.id.button);
 
-                RadioGroup rgType = (RadioGroup) findViewById(R.id.rgType);
-                if(rgType.getCheckedRadioButtonId() == R.id.rdbAdult)
-                    outputStr += getResources().getString(R.string.regularticket)+ "\n";
-                else if(rgType.getCheckedRadioButtonId() == R.id.rdbChild)
-                    outputStr += getResources().getString(R.string.childticket)+ "\n";
-                else
-                    outputStr += getResources().getString(R.string.studentticket)+ "\n";
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
 
-                TextView lblOutput = (TextView) findViewById(R.id.lblOutput);
+                int quantity = 0;
+                try {
+                    quantity = Integer.parseInt(editTextNumber.getText().toString());
+                } catch (NumberFormatException e) {
+                    lblOutput.setText("請輸入有效的購買張數");
+                    return;
+                }
+
+                if (quantity == 0) {
+                    lblOutput.setText("請輸入購買張數");
+                    return;
+                }
+
+                RadioButton rdbBoy = findViewById(R.id.rdbBoy);
+                RadioButton rdbGirl = findViewById(R.id.rdbGirl);
+                String gender = "";
+                if (rdbBoy.isChecked()) {
+                    gender = getResources().getString(R.string.male);
+                } else if (rdbGirl.isChecked()) {
+                    gender = getResources().getString(R.string.female);
+                }
+
+                RadioGroup rgType = findViewById(R.id.rgType);
+                int ticketPrice = 0;
+                String ticketType = "";
+                if (rgType.getCheckedRadioButtonId() == R.id.rdbAdult) {
+                    ticketType = getResources().getString(R.string.regularticket);
+                    ticketPrice = 500;
+                } else if (rgType.getCheckedRadioButtonId() == R.id.rdbChild) {
+                    ticketType = getResources().getString(R.string.childticket);
+                    ticketPrice = 250;
+                } else if (rgType.getCheckedRadioButtonId() == R.id.rdbStudent) {
+                    ticketType = getResources().getString(R.string.studentticket);
+                    ticketPrice = 400;
+                }
+
+                int totalCost = ticketPrice * quantity;
+
+                String outputStr = String.format("%s\n%s: %d 張\n金額 %d 元", gender, ticketType, quantity, totalCost);
                 lblOutput.setText(outputStr);
 
             }
         });
-    }
 
+        final Button confirmBtn = findViewById(R.id.confirmBtn);
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
 }
